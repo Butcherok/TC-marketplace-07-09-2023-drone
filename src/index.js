31660759f465cde1e826e7c3b9b82823b0c980d2
Butcherok <Butcherok231@gmail.com>
2023-10-17 18:33:09 +0300

    fix global style

--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { GlobalStyles } from 'styles/GlobalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/TC-marketplace-07-09-2023-drone">
+      <GlobalStyles />
       <App />
     </BrowserRouter>
   </React.StrictMode>
