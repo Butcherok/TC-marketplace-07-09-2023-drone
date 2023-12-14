95cb4ce7896560dd0d07f880ed19c5f297fdbda4
SergeyTokareff <sergey.v.tokareff@gmail.com>
2023-12-14 11:55:44 +0200

    test

--- src/components/NewProducts/NewProducts.jsx
+++ src/components/NewProducts/NewProducts.jsx
@@ -1,18 +1,22 @@
-import { useApi } from '../../contexts/ApiContext/ApiContext';
+// import { useApi } from '../../contexts/ApiContext/ApiContext';
+// import { Card } from './NewProducts.styled';
 
 const NewProducts = () => {
-  const { query } = useApi();
+  // const { query } = useApi();
 
   return (
-    <div className="api">
-      {query.map((item, index) => (
-        <div key={index}>
-          <img src={item.gallery[0]} alt="ddd" />
-          <h3>{item.title}</h3>
-          <p>{item.price}</p>
-        </div>
-      ))}
-    </div>
+    // <section>
+    //   <Card className="card">
+    //     {query.map((item, index) => (
+    //       <div key={index}>
+    //         <img src={item.gallery[0]} alt="ddd" />
+    //         <h3>{item.title}</h3>
+    //         <p>{item.price}</p>
+    //       </div>
+    //     ))}
+    //   </Card>
+    // </section>
+    <></>
   );
 };
 
