82371f2fb47444756c87d087a0504117656c7810
SergeyTokareff <sergey.v.tokareff@gmail.com>
2024-01-17 12:05:37 +0200

    edit discounts section

--- src/components/Discounts/Discounts.jsx
+++ src/components/Discounts/Discounts.jsx
@@ -1,6 +1,8 @@
+import { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
+import { NavLink } from 'react-router-dom';
 
 import { useApi } from '../../contexts/ApiContext/ApiContext';
 
@@ -14,6 +16,22 @@ import {
 const Discounts = () => {
   const { query } = useApi();
 
+  const [cardWidth, setCardWidth] = useState(146);
+
+  useEffect(() => {
+    const updateCardWidth = () => {
+      setCardWidth(window.innerWidth > 1439 ? 206 : 146);
+    };
+
+    window.addEventListener('resize', updateCardWidth);
+
+    updateCardWidth();
+
+    return () => {
+      window.removeEventListener('resize', updateCardWidth);
+    };
+  }, []);
+
   const settings = {
     className: 'slider variable-width',
     infinite: false,
@@ -22,15 +40,20 @@ const Discounts = () => {
     arrows: false,
   };
 
-  const cardWidth = 196;
-  const cardHeight = 248;
+  // const cardWidth = 146;
+  const cardHeight = 250;
+
+  const groupedQuery = [];
+  for (let i = 0; i < query.length; i += 2) {
+    groupedQuery.push(query.slice(i, i + 2));
+  }
 
   return (
-    <StyledDiscounts className='new-products'>
+    <StyledDiscounts className="discounts">
       <DiscountsContainer>
         <DiscountsTitle>Акції</DiscountsTitle>
         <Slider {...settings}>
-          {query.map((item, index) => (
+          {/* {query.map((item, index) => (
             <ProductCard
               key={index}
               title={item.title}
@@ -40,6 +63,28 @@ const Discounts = () => {
               width={cardWidth}
               height={cardHeight}
             />
+          ))} */}
+          {groupedQuery.map((group, index) => (
+            <div key={index} className="cards-group">
+              <div className="cards-group-head">
+                <span className="cards-head-title">Дрони</span>
+                <NavLink>Більше &#5171;</NavLink>
+              </div>
+              <div className="card-group-content">
+                {group.map((item, itemIndex) => (
+                  <ProductCard
+                    className="card1"
+                    key={itemIndex}
+                    title={item.title}
+                    img={item.img}
+                    price={item.price}
+                    _id={item._id}
+                    width={cardWidth}
+                    height={cardHeight}
+                  />
+                ))}
+              </div>
+            </div>
           ))}
         </Slider>
       </DiscountsContainer>
