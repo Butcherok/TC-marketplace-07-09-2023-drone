16d7f0ee553fa974153e07bb2b0993c9dc8f3c9b
SergeyTokareff <sergey.v.tokareff@gmail.com>
2024-02-27 16:21:49 +0200

    add authorization

--- src/components/UI/LoginForm/LoginForm.jsx
+++ src/components/UI/LoginForm/LoginForm.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { useState } from 'react';
+
 import Input from '../Input/Input';
 import {
   StyledLoginForm,
@@ -6,15 +9,76 @@ import {
   Message,
   LoginLink,
 } from './LoginForm.styled';
+import { useAuth } from '../../../contexts/AuthContext/AuthContext';
 
 const LoginForm = () => {
+  const { dispatch } = useAuth();
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const handleInputChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+    console.log(formData);
+  };
+
+  const handleFormSubmit = async e => {
+    e.preventDefault();
+
+    try {
+      const response = await axios.post(
+        'https://dr-one-marketplace.onrender.com/users/login',
+        formData,
+        {
+          auth: {
+            username: 'admin',
+            password: '123456',
+          },
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+      const userData = response.data.user.email;
+      const token = response.data.token;
+
+      console.log(response.data);
+      console.log('Token:', token);
+      dispatch({
+        type: 'LOGIN',
+        payload: {
+          user: userData,
+          token: token,
+        },
+      });
+      console.log('Ви авторизувались');
+      console.log('Token:', token);
+    } catch (error) {
+      console.error('Помилка авторизації', error);
+    }
+  };
+
   return (
     <>
       <Title>Авторізація</Title>
-      <StyledLoginForm>
-        <Input type="text" placeholder="Введіть email" />
-        <Input type="password" placeholder="Введіть пароль" />
-        <BtnSubmit>Увійти</BtnSubmit>
+      <StyledLoginForm onSubmit={handleFormSubmit}>
+        <Input
+          type="email"
+          name="email"
+          placeholder="Введіть email"
+          onChange={handleInputChange}
+        />
+        <Input
+          type="password"
+          name="password"
+          placeholder="Введіть пароль"
+          onChange={handleInputChange}
+        />
+        <BtnSubmit type="submit">Увійти</BtnSubmit>
       </StyledLoginForm>
       <Message>
         Якщо ви ще не зареєстровані, перейдіть на{' '}
