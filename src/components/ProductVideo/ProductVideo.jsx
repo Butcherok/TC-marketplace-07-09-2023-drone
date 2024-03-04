b9bcbeeb2511de073634722d6fb48ec63e35fc0f
Vasyl Shaposhnyk <aries240302@gmail.com>
2024-03-04 22:41:28 +0200

    Some changes in video section

--- src/components/ProductVideo/ProductVideo.jsx
+++ src/components/ProductVideo/ProductVideo.jsx
@@ -1,13 +1,31 @@
-import ReactPlayer from 'react-player/lazy';
+// import ReactPlayer from 'react-player/lazy';
+
+import '@vidstack/react/player/styles/base.css';
+import { MediaPlayer, MediaProvider } from '@vidstack/react';
+import { PlayIcon } from '@vidstack/react/icons';
 import video from './mavic.mp4';
 import { VideoBackground } from './ProductVideo.styled';
+import { useRef } from 'react';
+// import icon from '../../assets/icons/sprite.svg';
 
 const ProductVideo = () => {
-  ReactPlayer.removeCustomPlayers();
+  const player = useRef < MediaPlayerInstance > null;
+  const paused = useMediaState('paused', player);
+  //   const { paused } = useMediaStore(player);
+
   return (
     <>
       <VideoBackground>
-        <ReactPlayer url={video} controls={true} />
+        <MediaPlayer
+          src={video}
+          //   playing={playing}
+          width="100%"
+          height="100%"
+          ref={player}
+        >
+          <MediaProvider />
+          <PlayIcon size={40} />
+        </MediaPlayer>
       </VideoBackground>
     </>
   );
--- src/components/ProductVideo/ProductVideo.styled.jsx
+++ src/components/ProductVideo/ProductVideo.styled.jsx
@@ -1,8 +1,10 @@
 import styled from 'styled-components';
 
-export const VideoBackground = styled.div`
+export const VideoBackground = styled.section`
   margin-bottom: 40px;
 `;
+
+//
 // .player-wrapper {
 //   position: relative;
 //   padding-top: 56.25%; /* Player ratio: 100 / (1280 / 720) */
